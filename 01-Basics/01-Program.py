ce89c0c23a23e4f543a46a56b3f73aee7fbe8fec
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-10-04 11:55:52 +0530

    Add files via upload

---
+++ 01-Basics/01-Program.py
@@ -0,0 +1,13 @@
+a = 10
+b = 12
+c = a + b
+
+print("Sum is",c)
+
+print("Sum of", a, "and", b, "is", c)
+
+print("Sum of %d and %d is %d"%(a,b,c))
+
+print("Sum of {} and {} is {}".format(a,b,c))
+
+print(f"Sum of {a} and {b} is {c}")
---
+++ 01-Basics/02-Program.py
@@ -0,0 +1,10 @@
+# by default input() is type of string
+username = input("Enter your name : ")
+print("Hello",username)
+
+# type casting/conversion
+f_num = int(input("Enter first number : "))
+s_num = input("Enter second number : ")
+
+result = f_num + int(s_num)
+print("Sum is",result)
