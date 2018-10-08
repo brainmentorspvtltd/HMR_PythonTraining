d9fa25436c081dab1c9f85289b283b987a647466
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-10-08 13:31:05 +0530

    Add files via upload

--- Pygame/03-KeyEvents.py
+++ Pygame/03-KeyEvents.py
@@ -22,6 +22,18 @@ moveY = 0
 random_x = random.randrange(0, width - 50)
 random_y = random.randrange(0, height - 50)
 
+snakeList = []
+snakeLength = 1
+
+clock = pygame.time.Clock()
+FPS = 90
+
+def snake(snakeList):
+    for i in range(len(snakeList)):
+        pygame.draw.rect(screen,black,[snakeList[i][0],
+                                       snakeList[i][1],
+                                       50,50])
+
 while True:
 
     for event in pygame.event.get():
@@ -31,22 +43,23 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                moveX = 1
+                moveX = 6
                 moveY = 0
             elif event.key == pygame.K_LEFT:
-                moveX = -1
+                moveX = -6
                 moveY = 0
             elif event.key == pygame.K_DOWN:
-                moveY = 1
+                moveY = 6
                 moveX = 0
             elif event.key == pygame.K_UP:
-                moveY = -1
+                moveY = -6
                 moveX = 0
     
     screen.fill(red)
     
-    rect_1 = pygame.draw.rect(screen, black, [x,y,50,50])
+    #rect_1 = pygame.draw.rect(screen, black, [x,y,50,50])
     #rect_2 = pygame.draw.rect(screen, black, [random_x,random_y,50,50])
+    rect_1 = pygame.Rect(x,y,50,50)
     rect_2 = pygame.Rect(random_x,random_y,50,50)
 
     screen.blit(frog, (random_x,random_y))
@@ -54,10 +67,22 @@ while True:
     x += moveX
     y += moveY
 
+    snakeHead = []
+    snakeHead.append(x)
+    snakeHead.append(y)
+
+    snakeList.append(snakeHead)
+
+    if len(snakeList) > snakeLength:
+        del snakeList[0]
+
+    snake(snakeList)
+
     if rect_1.colliderect(rect_2):
         #print("Collision detection")
         random_x = random.randrange(0, width - 50)
         random_y = random.randrange(0, height - 50)
+        snakeLength += 5
 
     if x > width:
         moveX = -1
@@ -69,6 +94,7 @@ while True:
         moveY = 1
     
     pygame.display.update()
+    clock.tick(FPS)
 
 
 
