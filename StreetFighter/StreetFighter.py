ce5b09c9d2864c28b18c792216e389735901514e
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-10-12 13:47:53 +0530

    Add files via upload

--- StreetFighter/StreetFighter.py
+++ StreetFighter/StreetFighter.py
@@ -19,9 +19,23 @@ class Spritesheet():
 
 class Player_1(pygame.sprite.Sprite):
     walking_frames = []
+    standingFrames = []
+    punchFrames = []
+    kickFrames = []
+
     def __init__(self):
         super().__init__()
         spritesheet = Spritesheet(player_sprite)
+
+        self.image = spritesheet.getImage(48, 247, 106, 239)
+        self.standingFrames.append(self.image)
+        self.image = spritesheet.getImage(264, 240, 113, 249)
+        self.standingFrames.append(self.image)
+        self.image = spritesheet.getImage(482, 248, 106, 237)
+        self.standingFrames.append(self.image)
+        self.image = spritesheet.getImage(687, 247, 112, 241)
+        self.standingFrames.append(self.image)
+
         self.image = spritesheet.getImage(42, 247, 116, 243)
         self.walking_frames.append(self.image)
         self.image = spritesheet.getImage(44, 731, 118, 243)
@@ -29,7 +43,28 @@ class Player_1(pygame.sprite.Sprite):
         self.image = spritesheet.getImage(256, 731, 118, 243)
         self.walking_frames.append(self.image)
 
+        self.rect = self.image.get_rect()
+        self.rect.center = (width / 2 - 250, height / 2 + 70)
+
+        self.pos = 0
+
+    def update(self):
+        self.pos += 5
+
+        frame = (self.pos // 30) % len(self.standingFrames)
+        # print("Position",self.pos//30)
+        # print("Frame",frame)
+        self.image = self.standingFrames[frame]
+
 player_sprite = pygame.image.load("images/ken_.png")
+
+all_sprites = pygame.sprite.Group()
+player = Player_1()
+
+ken = pygame.sprite.Group()
+ken.add(player)
+
+all_sprites.add(player)
 
 while True:
     for event in pygame.event.get():
@@ -37,4 +72,8 @@ while True:
             pygame.quit()
             quit()
     screen.blit(background,(0,0))
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+
     pygame.display.update()
